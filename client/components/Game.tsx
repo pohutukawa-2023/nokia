57e458bdc39642cafe713c472aee4bf7aa69e3a3
Pathik Modi <modipathik95@gmail.com>
2023-11-03 10:23:28 +1300

    add grid on main game page

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Game() {
+  const numRows = 20
+  const numCols = 100
+
+  const [grid, setGrid] = useState<number[][]>([])
+
+  useEffect(() => {
+    const newGrid = []
+    for (let i = 0; i < numRows; i++) {
+      const row = []
+      for (let j = 0; j < numCols; j++) {
+        row.push(0)
+      }
+      newGrid.push(row)
+    }
+    setGrid(newGrid)
+  }, [])
+
   return (
     <div>
       <h2>Main Gameplay Page</h2>
-      <div></div>
+      <div>
+        <table>
+          <tbody>
+            {grid.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, colIndex) => (
+                  <td key={colIndex} className="gridCell">
+                    .
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div>
         <Link to={'/'}>
           <button>Go Back</button>
